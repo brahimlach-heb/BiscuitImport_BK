bd2b6881c75426a430fac738f47ed55b7a9f259c
brahim lachheb <brahimlachheb09@gmail.com>
2026-01-30 01:26:43 +0100

    test local is work

--- src/config/env.js
+++ src/config/env.js
@@ -5,7 +5,7 @@ dotenv.config({ path: path.resolve(process.cwd(), '.env') });
 
 module.exports = {
   PORT: process.env.PORT || 3000,
-  BASE_URL: 'http://72.62.237.60',
+  BASE_URL: '',
   NODE_ENV: process.env.NODE_ENV || 'development',
   JWT_SECRET: process.env.JWT_SECRET || 'change_me',
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || '1d',
