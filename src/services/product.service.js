fc52ea5e10060cbba3673714c15da02c33c8042a
brahim lachheb <brahimlachheb09@gmail.com>
2026-01-30 02:08:45 +0100

    add roles api 28

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -100,7 +100,7 @@ const getAllProducts = async (filter, roleId, roleCode) => {
       if (product.flavors && Array.isArray(product.flavors)) {
         product.flavors = product.flavors.map(flavor => {
           const imageUrl = flavor.image && !flavor.image.startsWith('http') 
-            ? `${BASE_URL}${flavor.image.startsWith('/') ? '' : '/'}${flavor.image}`
+            ? `${flavor.image.startsWith('/') ? '' : '/'}${flavor.image}`
             : flavor.image;
           
           return {
